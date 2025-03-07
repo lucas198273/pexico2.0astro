192d81a4fb72ac4a70338471e6d162d1e317a103
Lucas Dias <lucasinga2000@gmail,com>
2025-03-07 12:25:34 -0300

    0

--- public/scripts/sobre.js
+++ public/scripts/sobre.js
@@ -1,22 +1,17 @@
-const openModal = document.getElementById("open-modal-horarios");
+
 const openModalMobile = document.getElementById("open-modal-horarios-mobile");
 const mobileMenu = document.getElementById("mobile-menu");
-const modalHorarios = document.getElementById("modal-horarios");
+
 const menuButton = document.getElementById("menu-button");
 
-openModal.addEventListener("click", () => {
-    modalHorarios.classList.remove("hidden");
-  });
 
-openModalMobile.addEventListener("click", () => {
-    modalHorarios.classList.remove("hidden");
-  });
+
 
   
   menuButton.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
   });
-  
+
   const mobileMenuItems = mobileMenu.querySelectorAll("a, button");
   mobileMenuItems.forEach(item => {
     item.addEventListener("click", () => {
